9b96b6f3bc1b9a6e3c8b0f9c574ced0e1ccd21c6
SAH <143020123+BRINAHH@users.noreply.github.com>
2024-02-28 11:21:39 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,8 @@
 const calcular = () => { // cantando o numero para calcular 
     let numero1 = document.getElementById("numero1");//LET atribuiu o nomes nos calculos e igual a funcao que identifica um ID
-    let numero2 = document.getElementById("numero2");
+    let numero2 = document.getElementById("numero2");//LET atribuiu o nomes nos calculos e igual a funcao que identifica um ID
     let resultado = document.getElementById("resultado");
-    //determinou o resultado
+    //Na linha acima determinou o resultado 
 
     // o resultado do valor e igual ao numero do valor mais o numero do valor 2 
     resultado.value = parseInt(numero.value) + parseInt(numero2.value)
