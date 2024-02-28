4c17a411cb1c228c058a2a9f39e3341c8eed88a0
SAH <143020123+BRINAHH@users.noreply.github.com>
2024-02-28 11:03:26 -0300

    Update trycatch.js

--- trycatch.js
+++ trycatch.js
@@ -6,10 +6,10 @@ try { //TENTANDO colocar valores na variavel1, e inprimindo no console o recado
     variaval1 = "valor nao permitido";
     console.log("variavel1 foi alterada para:", variaval1);
 } catch (e) { // colocar/pegar o recado no console 
-    console.error("ops! ocorreu um erro:", e);
+    console.error("ops! ocorreu um erro:", e); 
 }
 
-try {//com os mesmos codigos na variavel2
+try {//TENTANDO colocar valores na variavel2, assim estara no console informacoes sobre o console
     variaval2 = "valor permitido";
     console.log(" a variavel2 foi alterada para:", variaval2);
 } catch (e) {
